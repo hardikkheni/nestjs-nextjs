f139e1adca76c856be04d37f386dc8659b5db957
HardikKheni <hardikkheni.work@gmail.com>
2021-09-13 13:40:09 +0530

    verified hydrating is working

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <h1>Hello, World!</h1>;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <div style={{ textAlign: 'center', verticalAlign: 'middle' }}>
+        <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
